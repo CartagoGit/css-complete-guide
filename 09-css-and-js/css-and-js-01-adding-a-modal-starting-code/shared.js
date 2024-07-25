dc6aa9d2c595ed238a5fe53823ae44a53f45ef63
Cartago <cartago.relaxingcup@gmail.com>
2024-07-25 14:10:21 +0200

    Creamos un menu de navegacion para movil y le añadimos js

--- 09-css-and-js/css-and-js-01-adding-a-modal-starting-code/shared.js
+++ 09-css-and-js/css-and-js-01-adding-a-modal-starting-code/shared.js
@@ -2,18 +2,36 @@ const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 
 const planButtons = document.querySelectorAll('#plans button');
-const [free, recomended, premium] = planButtons;
+const [freeButton, recomendedButton, premiumButton] = planButtons;
+
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
+
+const modalNegativeButton = modal.querySelector('button');
 
 planButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        modal.style.display = 'block';
-		backdrop.style.display = 'block';
+	button.addEventListener('click', () => {
+		// modal.style.display = 'block';
+		// backdrop.style.display = 'block';
+		modal.classList.add('open');
+		backdrop.classList.add('open');
 	});
 });
 
+[modalNegativeButton, backdrop].forEach((element) =>
+	element.addEventListener('click', () => {
+		// modal.style.display = 'none';
+		// backdrop.style.display = 'none';
+		// mobileNav.style.display = 'none';
+		modal.classList.remove('open');
+		backdrop.classList.remove('open');
+		mobileNav.classList.remove('open');
+	})
+);
 
-const modalNegativeButtons = modal.querySelector('button');
-modalNegativeButtons.addEventListener('click', () => {
-	modal.style.display = 'none';
-	backdrop.style.display = 'none';
+toggleButton.addEventListener('click', () => {
+	// mobileNav.style.display = 'block';
+	// backdrop.style.display = 'block';
+	mobileNav.classList.add('open');
+	backdrop.classList.add('open');
 });
